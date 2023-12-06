6b22a4a8ee7f61d7d86173b3e489ae71530d0717
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-06 21:48:48 +0700

    tinh_dien_tich

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float chieudai;
+        float chieurong;
+        float dientich;
+        Console.WriteLine("NHAP CHIEU DAI : ");
+        chieudai = float.Parse(Console.ReadLine());
+        Console.WriteLine("NHAP CHIEU RONG : ");
+        chieurong = float.Parse(Console.ReadLine());
+        dientich = chieudai * chieurong;
+        Console.WriteLine("dien tich = "+dientich);
+}
+}
